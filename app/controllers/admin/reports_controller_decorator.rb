e30ac4ce526bb9227c4d0db495c281c3d618f546
Phu Le <phule@vinasource.com>
2014-04-02 15:06:11 +0700

    comment out list report

--- app/controllers/admin/reports_controller_decorator.rb
+++ app/controllers/admin/reports_controller_decorator.rb
@@ -1,5 +1,5 @@
 Admin::ReportsController.class_eval do
-  AVAILABLE_REPORTS.merge({:foo_report => {:name => I18n.t(:foo_name), :description => I18n.t(:foo_description)}})
+  #AVAILABLE_REPORTS.merge({:foo_report => {:name => I18n.t(:foo_name), :description => I18n.t(:foo_description)}})
 
   def foo_report
     # Add your logic here
